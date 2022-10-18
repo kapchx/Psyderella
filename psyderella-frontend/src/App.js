ae14336b7e931e4c6a464a71865d70bcb857dc65
Murat Koçak <dylrgz@inf.elte.hu>
2022-10-18 16:15:06 +0200

    Dislike button created and added to project

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
+import Dislike from "./components/actions/Dislike"
 
 const App = () => {
   return (
     <div className="app">
         <Header/>
+        <Dislike/>
     </div>
 )
 }
