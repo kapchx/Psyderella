32d5cd62982f1443fa7cd0c38e2d4b78834a4257
Murat Koçak <dylrgz@inf.elte.hu>
2022-10-18 16:55:28 +0200

    Rewind button created and added to project

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -2,14 +2,16 @@ import React from "react"
 import Header from "./components/Header"
 import Dislike from "./components/actions/Dislike"
 import Like from "./components/actions/Like"
+import Rewind from "./components/actions/Rewind"
 
 const App = () => {
   return (
     <div className="app">
-        <Header/>
-        <Dislike/>
-        <Like/>
+      <Header />
+      <Dislike />
+      <Like />
+      <Rewind />
     </div>
-)
+  )
 }
 export default App;
