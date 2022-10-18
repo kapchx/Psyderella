593b310acb139d51f89af3956d957f19c717146f
Murat Koçak <dylrgz@inf.elte.hu>
2022-10-18 16:43:19 +0200

    Like button created and added to project, Dislike.js file reorganized

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
 import Dislike from "./components/actions/Dislike"
+import Like from "./components/actions/Like"
 
 const App = () => {
   return (
     <div className="app">
         <Header/>
         <Dislike/>
+        <Like/>
     </div>
 )
 }
