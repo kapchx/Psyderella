585a4b47a5dfdaceee6a7948d2212e1d51e1436e
Berkes-Scherer David <kapchx@inf.elte.hu>
2022-11-13 15:07:00 +0100

    Added css to AuthModal component

--- psyderella-frontend/src/components/AuthModal.js
+++ psyderella-frontend/src/components/AuthModal.js
@@ -2,10 +2,10 @@ import { useState } from 'react'
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
 
-    const [ eamil, setEmail ] = useState(null)
-    const [ password, setPassword ] = useState(null)
-    const [ confirmPassword, setConfirmPassword ] = useState(null)
-    const [ error, setError ] = useState(null)
+    const [eamil, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
 
     const handleClick = () => {
         setShowModal(false)
@@ -13,8 +13,8 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        try{
-            if( isSignUp && ( password !== confirmPassword)){
+        try {
+            if (isSignUp && (password !== confirmPassword)) {
                 setError('Passwords need to match!')
             }
             console.log('make a post request to our database')
@@ -26,43 +26,43 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
 
     return (
         <div className="auth-modal">
-        <div className="close-icon" onClick={handleClick}>ⓧ</div>
+            <div className="close-icon" onClick={handleClick}>ⓧ</div>
 
-        <h2>{isSignUp ? 'CREATE ACCOUNT': 'LOG IN'}</h2>
-        <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="email"
-                required={true}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="password"
-                required={true}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            {isSignUp && <input
-                type="password"
-                id="password-check"
-                name="password-check"
-                placeholder="confirm password"
-                required={true}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-            />}
-            <input className="secondary-button" type="submit"/>
-            <p>{error}</p>
-        </form>
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+            <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {isSignUp && <input
+                    type="password"
+                    id="password-check"
+                    name="password-check"
+                    placeholder="confirm password"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />}
+                <input className="secondary-button" type="submit" />
+                <p>{error}</p>
+            </form>
 
-        <hr/>
-        <h2>GET THE APP</h2>
+            <hr />
+            <h2>GET THE APP</h2>
 
-    </div>
+        </div>
     )
 }
 
