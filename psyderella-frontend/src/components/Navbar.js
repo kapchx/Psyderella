6ac4cf49326dd54fa9b2fdd0e0dac7135afc4eee
Berkes-Scherer David <kapchx@inf.elte.hu>
2022-11-13 16:08:31 +0100

    Onboarding form created

--- psyderella-frontend/src/components/Navbar.js
+++ psyderella-frontend/src/components/Navbar.js
@@ -1,3 +1,4 @@
+
 const Navbar = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
 
     const handleClick = () => {
@@ -10,7 +11,7 @@ const Navbar = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) =>
         
         <nav>
         <dim className="logo-container">
-            <img className="logo" src={minimal ? "https://policies.tinder.com/static/ad5fe8e55f591b89e6ccd5fce88a5179/906b5/tinder_white_flame_black_outline.png" : "https://1000logos.net/wp-content/uploads/2018/07/tinder-app-logo.jpg"} />
+            <img className="logo" src="images/Psyderella_logo.png" />
         </dim>
         {!authToken && <button 
             onClick={handleClick} 
--- psyderella-frontend/src/pages/OnBoarding.js
+++ psyderella-frontend/src/pages/OnBoarding.js
@@ -1,6 +1,126 @@
+import { useState } from 'react';
+import Navbar from "../components/Navbar"
+
 const Onboarding = () => {
+
+    const handleChange = () => {
+        console.log("changes")
+    }
+
+    const handleSubmit = () => {
+        console.log("submitted")
+    }
+
     return (
-        <div>Onboarding</div>
+        <>
+            <Navbar
+                minimal={true}
+                setShowModal={() => { }}
+                showModal={false}
+            />
+            <div className="onboarding">
+                <h2>CREATE ACCOUNT</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label thmlFor="first_name">First Name</label>
+                        <input
+                            id="first_name"
+                            type="text"
+                            name="first_name"
+                            placeholder="First name"
+                            required={true}
+                            value={handleChange}
+                        />
+
+                        <label>Birthday</label>
+                        <div className="multiple-input-container">
+                            <input
+                                id="dob_day"
+                                type="number"
+                                name="dob_day"
+                                placeholder="DD"
+                                required={true}
+                                value={handleChange}
+                            />
+                            <input
+                                id="dob_month"
+                                type="number"
+                                name="dob_month"
+                                placeholder="MM"
+                                required={true}
+                                value={handleChange}
+                            />
+                            <input
+                                id="dob_year"
+                                type="number"
+                                name="dob_year"
+                                placeholder="YYYY"
+                                required={true}
+                                value={handleChange}
+                            />
+                        </div>
+                        <label>Gender</label>
+                        <div className="multiple-input-container">
+                            <label>Man</label>
+                            <input
+                                id="man_gender"
+                                type="radio"
+                                name="gender"
+                                value="man"
+                                onChange={handleChange}
+                            />
+                            <label>Woman</label>
+                            <input
+                                id="women_gender"
+                                type="radio"
+                                name="gender"
+                                value="woman"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <label>Show me</label>
+                        <div className="multiple-input-container">
+                            <label>Man</label>
+                            <input
+                                id="man_gender_interes"
+                                type="radio"
+                                name="gender_interest"
+                                value="man"
+                                onChange={handleChange}
+                            />
+                            <label>Woman</label>
+                            <input
+                                id="women_gender_interest"
+                                type="radio"
+                                name="gender_interest"
+                                value="woman"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <label>About me</label>
+                        <input
+                            id="about"
+                            type="text"
+                            name="about"
+                            required={true}
+                            placeholder="I like long walks"
+                            value={""}
+                            onChange={handleChange}
+                        />
+                        <label>Profile Picture</label>
+                        <input
+                            type="url"
+                            name="url"
+                            id="url"
+                            onChange={handleChange}
+                            required={true}
+                        />
+
+                    </section>
+                </form>
+            </div>
+        </>
     )
 }
 
