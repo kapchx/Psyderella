7c3531e42143771e891947c2e7ee67fc44b00863
Berkes-Scherer David <kapchx@inf.elte.hu>
2022-11-14 10:35:33 +0100

    Update App.js

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import OnBoarding from "./pages/OnBoarding";
