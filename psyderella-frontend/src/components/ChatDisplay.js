5883929feb5d4ec6116cb5b749e26878f1954e24
Neme98 <ora5fm@inf.elte.hu>
2022-12-16 20:27:26 +0100

    chatdisplay frontend settings and connect with backend

--- psyderella-frontend/src/components/ChatDisplay.js
+++ psyderella-frontend/src/components/ChatDisplay.js
@@ -7,31 +7,73 @@ const ChatDisplay = ({user,clickedUser}) => {
   const userId = user?.user_id
   const clickedUserId =clickedUser?.user_id
   const [usersMessages, setUsersMessages] = useState(null)
+  const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
 
-  const GetUsersMessages = async () => {
-    try{
-        const response = await axios.get('http://localhost:8000/messages', {
-                params:{userId: userId, correspondingUserId: clickedUserId}
-        })
-        setUsersMessages(response.data)
-    } catch(error) {
-        console.log(error)
+  const getUsersMessages = async () => {
+
+        try{
+            const response = await axios.get('http://localhost:8000/messages', {
+                    params:{userId: userId, correspondingUserId: clickedUserId}
+            })
+            setUsersMessages(response.data)
+        } catch(error) {
+            console.log(error)
+        }
+
     }
 
+    const getClickedUserMessages = async () => {
+
+            try{
+                const response = await axios.get('http://localhost:8000/messages', {
+                        params:{userId: clickedUserId, correspondingUserId: userId}
+                })
+                setClickedUsersMessages(response.data)
+            } catch(error) {
+                console.log(error)
+            }
+
+        }
+
     useEffect(()=>{
-        GetUsersMessages()
-    }, [usersMessages])
+                    getUsersMessages()
+                    getClickedUserMessages()
+                }, [usersMessages, clickedUsersMessages])
 
-    console.log(usersMessages)
 
-  }
+
+    const messages = []
+    console.log('usersMessages', usersMessages)
+
+    usersMessages?.forEach(message => {
+            const formattedMessage = {}
+            formattedMessage['name'] = user?.first_name
+            formattedMessage['img'] = user?.url
+            formattedMessage['message'] = message.message
+            formattedMessage['timestamp'] = message.timestamp
+            messages.push(formattedMessage)
+        })
+
+    clickedUsersMessages?.forEach(message => {
+            const formattedMessage = {}
+            formattedMessage['name'] = clickedUser?.first_name
+            formattedMessage['img'] = clickedUser?.url
+            formattedMessage['message'] = message.message
+            formattedMessage['timestamp'] = message.timestamp
+            messages.push(formattedMessage)
+        })
+
+    const descendingOrderMessages = messages?.sort((a,b) => a.timestamp.localeCompare(b.timestamp))
+
+
 
   return (
     <>
-      <Chat />
+      <Chat descendingOrderMessages={descendingOrderMessages}/>
       <ChatInput />
     </>
   );
+
 }
 
 export default ChatDisplay;
